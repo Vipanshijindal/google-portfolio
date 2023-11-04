6e11eb425f8d939cae602ef069f9d29945d76aba
Vipanshi Jindal <136837281+Vipanshijindal@users.noreply.github.com>
2023-11-04 10:20:47 +0530

    google resume files are uploaded on github

---
+++ googleresume.js
@@ -0,0 +1,42 @@
+var typed = new Typed(".text", {
+    strings: ["p"],
+    typeSpeed: 100,
+    backSpeed: 500,
+    backDelay: 1000,
+    loop: true,
+  });
+  var typed = new Typed(".tex", {
+    strings: ["a"],
+    typeSpeed: 200,
+    backSpeed: 500,
+    backDelay: 1000,
+    loop: true,
+  });
+  var typed = new Typed(".te", {
+    strings: ["n"],
+    typeSpeed: 300,
+    backSpeed: 400,
+    backDelay: 1000,
+    loop: true,
+  });
+  var typed = new Typed(".t", {
+    strings: ["s"],
+    typeSpeed: 400,
+    backSpeed: 300,
+    backDelay: 1000,
+    loop: true,
+  });
+  var typed = new Typed(".texts", {
+    strings: ["h"],
+    typeSpeed: 500,
+    backSpeed: 200,
+    backDelay: 1000,
+    loop: true,
+  });
+  var typed = new Typed(".blu", {
+    strings: ["i"],
+    typeSpeed: 600,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
